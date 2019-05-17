471c78b31638c03bfd7bd3fb84ffe0e22d3a2ee0
zhangzhentao <tntcpu@126.com>
2019-05-17 18:06:18 +0800

    20190516_02 commit!

--- src/main/java/com/tcps/self/java8/chap7/ParallelStreams.java
+++ src/main/java/com/tcps/self/java8/chap7/ParallelStreams.java
@@ -27,12 +27,27 @@ public class ParallelStreams {
 
 	public static void main(String[] args) {
 		System.out.println("Sequential sum done in:"+
-				measureSumPerf(ParallelStreams::sequentialSum,10_000_000) + "msecs");
+				measureSumPerf(ParallelStreams::iterativeSum,10_000_000) + "msecs");
 	}
 
 	public static long sequentialSum(long n){
 		return Stream.iterate(1L, i -> i+1)
 				.limit(n)
 				.reduce(0L,Long::sum);
+	}
+
+	public static long parallelSum(long n){
+		return Stream.iterate(1L,i -> i+1)
+				.limit(n)
+				.parallel()
+				.reduce(0L,Long::sum);
+	}
+
+	public static long iterativeSum(long n){
+		long result = 0 ;
+		for (long i = 1L; i <= n ; i++) {
+			result += i;
+		}
+		return result;
 	}
 }
